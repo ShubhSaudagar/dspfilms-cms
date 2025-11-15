4f1109c5c073e613566ac4e1625cf75221642afa
ShubhSaudagar <shubhsaudagar@gmail.com>
2025-11-15 10:16:56 +0530

    Update Users.js

--- src/payload/collections/Users.js
+++ src/payload/collections/Users.js
@@ -1,1 +1,16 @@
-
+export default {
+  slug: "users",
+  auth: true,
+  admin: {
+    useAsTitle: "email",
+  },
+  fields: [
+    { name: "name", type: "text", required: true },
+    {
+      name: "role",
+      type: "select",
+      options: ["admin", "editor"],
+      defaultValue: "admin",
+    },
+  ],
+};
